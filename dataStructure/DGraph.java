d95f8b405be2b175929af0df7cbc04165123d73a
NDM-123 <57434608+NDM-123@users.noreply.github.com>
2020-01-05 19:31:19 +0200

    data Structures final

--- dataStructure/DGraph.java
+++ dataStructure/DGraph.java
@@ -1,73 +1,122 @@
 package dataStructure;
 
-import java.util.Collection;
+import java.io.Serializable;
+import java.util.*;
 
-public class DGraph implements graph{
+public class DGraph implements graph, Serializable {
+    public HashMap<Integer, node_data> vertices = new HashMap<Integer, node_data>();
+    ;
+    public HashMap<node_data, HashMap<Integer, edge_data>> edges = new HashMap<node_data, HashMap<Integer, edge_data>>();
+    int MC = 0;
+    int edgesSize = 0;
 
-	@Override
-	public node_data getNode(int key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public DGraph() {
+        this.vertices = new HashMap<Integer, node_data>();
+        this.edges = new HashMap<node_data, HashMap<Integer, edge_data>>();
+    }
 
-	@Override
-	public edge_data getEdge(int src, int dest) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public node_data getNode(int key) {
+        return this.vertices.get(key);
+    }
 
-	@Override
-	public void addNode(node_data n) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public edge_data getEdge(int src, int dest) {
+        node_data vertex = this.vertices.get(src);
+        edge_data edge = this.edges.get(vertex).get(dest);
 
-	@Override
-	public void connect(int src, int dest, double w) {
-		// TODO Auto-generated method stub
-		
-	}
+        return edge;
+    }
 
-	@Override
-	public Collection<node_data> getV() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void addNode(node_data n) {
+        if (this.vertices.containsKey(n) == false) {
+            this.vertices.put(n.getKey(), n);
+            this.edges.put(n, new HashMap<Integer, edge_data>());
+            this.MC++;
+        }
+    }
 
-	@Override
-	public Collection<edge_data> getE(int node_id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void connect(int src, int dest, double w) {
+        try {
+            node_data vertex_a = this.vertices.get(src);
+            node_data vertex_b = this.vertices.get(dest);
+            if (vertex_a == null || vertex_b == null)
+                throw new IllegalArgumentException("source vertex not in graph");
+            this.edges.get(vertex_a).put(dest, new edgeData(src, dest, w, "", 0));
+            this.MC++;
+            this.edgesSize++;
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-	@Override
-	public node_data removeNode(int key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Collection<node_data> getV() {
+        Collection<node_data> ver;
+        ver = this.vertices.values();
+        return ver;
+    }
 
-	@Override
-	public edge_data removeEdge(int src, int dest) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Collection<edge_data> getE(int node_id) {
 
-	@Override
-	public int nodeSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+        node_data vertex = this.vertices.get(node_id);
+        Collection<edge_data> edg = this.edges.get(vertex).values();
+        return edg;
+    }
 
-	@Override
-	public int edgeSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public node_data removeNode(int key) {
+        node_data vertex = this.vertices.get(key);
+        if (vertex == null) return null;
+        Set<node_data> set = this.edges.keySet();
+        for (node_data node_data : set) {
+            edge_data edg = this.edges.get(node_data).remove(key);
+            if (edg != null) this.edgesSize--;
+        }
+        this.edgesSize -= this.edges.get(vertex).size();
+        this.edges.remove(vertex);
+        this.MC++;
+        return this.vertices.remove(key);
 
-	@Override
-	public int getMC() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    }
+
+
+    @Override
+    public edge_data removeEdge(int src, int dest) {
+        node_data vertex = this.vertices.get(src);
+        edge_data edge = this.edges.get(vertex).remove(dest);
+        this.MC++;
+        return edge;
+    }
+
+
+    @Override
+    public int nodeSize() {
+        return this.vertices.size();
+    }
+
+    @Override
+    public int edgeSize() {
+        return this.edgesSize;
+    }
+
+    @Override
+    public int getMC() {
+        return MC;
+    }
+
+    public boolean equals(Object ob) {
+        if (!(ob instanceof DGraph)) return false;
+        boolean flag = false;
+        DGraph dg = (DGraph) ob;
+        if (!(this.vertices.equals(dg.vertices))) return false;
+        if (!(this.edges.equals(dg.edges))) return false;
+
+        return true;
+    }
+
 
 }
